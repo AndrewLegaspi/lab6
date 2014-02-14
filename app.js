f6d4f8b15ab3ad0f2063d0445e1efeb3ac9c4a93
vagrant <vagrant@precise32.(none)>
2014-02-14 01:52:05 +0000

    ajax lab

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 
+
+
+
 /**
  * Module dependencies.
  */
@@ -12,6 +15,7 @@ var index = require('./routes/index');
 var project = require('./routes/project');
 // Example route
 // var user = require('./routes/user');
+var user = require('./routes/palette');
 
 var app = express();
 
@@ -38,9 +42,12 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/project/:id', project.projectInfo);
+app.get('/palette', user.randomPalette);
 // Example route
 // app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+
